c3b5a75af839f9c5486e70eeed46e5d1c613c85c
blankaex <blankaex@protonmail.com>
2026-04-19 00:28:37 +0900

    added static shop data

---
+++ data/get_shop.php
@@ -0,0 +1,186 @@
+{
+  "status": "true",
+  "code": "200",
+  "messages": "",
+  "result": {
+    "東京都": [
+      {
+        "area": "銀座・赤坂・六本木エリア",
+        "list": [
+          {
+            "shop_type": "",
+            "shop_code": "614",
+            "shop_name": "銀座店",
+            "app_flg": "1",
+            "image": "http://cloud2.mue.lequeios.net/image.php?id=13&t=5"
+          },
+          {
+            "shop_type": "",
+            "shop_code": "609",
+            "shop_name": "赤坂店",
+            "app_flg": "1",
+            "image": "http://cloud2.mue.lequeios.net/image.php?id=15&t=5"
+          },
+          {
+            "shop_type": "",
+            "shop_code": "618",
+            "shop_name": "六本木店",
+            "app_flg": "1",
+            "image": "http://cloud2.mue.lequeios.net/image.php?id=14&t=5"
+          }
+        ]
+      },
+      {
+        "area": "渋谷エリア",
+        "list": [
+          {
+            "shop_type": "",
+            "shop_code": "611",
+            "shop_name": "渋谷店",
+            "app_flg": "1",
+            "image": "http://cloud2.mue.lequeios.net/image.php?id=6&t=5"
+          },
+          {
+            "shop_type": "",
+            "shop_code": "607",
+            "shop_name": "MODIS渋谷店",
+            "app_flg": "1",
+            "image": "http://cloud2.mue.lequeios.net/image.php?id=7&t=5"
+          }
+        ]
+      },
+      {
+        "area": "新宿エリア",
+        "list": [
+          {
+            "shop_type": "",
+            "shop_code": "610",
+            "shop_name": "新宿本店",
+            "app_flg": "1",
+            "image": "http://cloud2.mue.lequeios.net/image.php?id=1&t=5"
+          },
+          {
+            "shop_type": "",
+            "shop_code": "612",
+            "shop_name": "新宿靖国通り店",
+            "app_flg": "1",
+            "image": "http://cloud2.mue.lequeios.net/image.php?id=2&t=5"
+          }
+        ]
+      },
+      {
+        "area": "池袋エリア",
+        "list": [
+          {
+            "shop_type": "",
+            "shop_code": "613",
+            "shop_name": "池袋本店",
+            "app_flg": "1",
+            "image": "http://cloud2.mue.lequeios.net/image.php?id=4&t=5"
+          },
+          {
+            "shop_type": "",
+            "shop_code": "617",
+            "shop_name": "池袋西口店",
+            "app_flg": "1",
+            "image": "http://cloud2.mue.lequeios.net/image.php?id=5&t=5"
+          }
+        ]
+      },
+      {
+        "area": "上野エリア",
+        "list": [
+          {
+            "shop_type": "",
+            "shop_code": "619",
+            "shop_name": "上野公園前店",
+            "app_flg": "1",
+            "image": "http://cloud2.mue.lequeios.net/image.php?id=8&t=5"
+          },
+          {
+            "shop_type": "",
+            "shop_code": "608",
+            "shop_name": "上野御徒町店",
+            "app_flg": "1",
+            "image": "http://cloud2.mue.lequeios.net/image.php?id=9&t=5"
+          }
+        ]
+      },
+      {
+        "area": "秋葉原エリア",
+        "list": [
+          {
+            "shop_type": "",
+            "shop_code": "605",
+            "shop_name": "AKIBAマルチエンターテインメント店",
+            "app_flg": "1",
+            "image": "http://cloud2.mue.lequeios.net/image.php?id=10&t=5"
+          },
+          {
+            "shop_type": "",
+            "shop_code": "606",
+            "shop_name": "秋葉原電気街店",
+            "app_flg": "1",
+            "image": "http://cloud2.mue.lequeios.net/image.php?id=11&t=5"
+          },
+          {
+            "shop_type": "",
+            "shop_code": "604",
+            "shop_name": "秋葉原昭和通り館",
+            "app_flg": "1",
+            "image": "http://cloud2.mue.lequeios.net/image.php?id=12&t=5"
+          }
+        ]
+      }
+    ],
+    "神奈川県": [
+      {
+        "area": "",
+        "list": [
+          {
+            "shop_type": "",
+            "shop_code": "616",
+            "shop_name": "横浜関内店",
+            "app_flg": "1",
+            "image": "http://cloud2.mue.lequeios.net/image.php?id=17&t=5"
+          },
+          {
+            "shop_type": "",
+            "shop_code": "615",
+            "shop_name": "横浜ハマボールイアス店",
+            "app_flg": "1",
+            "image": "http://cloud2.mue.lequeios.net/image.php?id=16&t=5"
+          }
+        ]
+      }
+    ],
+    "": [
+      {
+        "area": "",
+        "list": [
+          {
+            "shop_type": "",
+            "shop_code": "602",
+            "shop_name": "天王寺店",
+            "app_flg": "1",
+            "image": "http://cloud2.mue.lequeios.net/image.php?id=19&t=5"
+          }
+        ]
+      }
+    ],
+    "大阪府": [
+      {
+        "area": "",
+        "list": [
+          {
+            "shop_type": "",
+            "shop_code": "620",
+            "shop_name": "なんば道頓堀店",
+            "app_flg": "1",
+            "image": "http://cloud2.mue.lequeios.net/image.php?id=18&t=5"
+          }
+        ]
+      }
+    ]
+  }
+}
